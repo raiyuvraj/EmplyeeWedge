97c0c1a550a30bbac2671ab218654987a056a894
raiyuvraj <yuvirai017@gmail.com>
2022-12-06 17:47:07 -0800

    [Yuvraj]- Emp Attendence

--- EmplyeeWedge/Program.cs
+++ EmplyeeWedge/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Full_Time = 1;
+        Random random = new Random();
+        int EmpCheck = random.Next();
+        if (EmpCheck == 1)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+    }
+ 
+}
