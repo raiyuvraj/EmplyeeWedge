9efc03a8bcee2c9032456c5e64fea0afa87e078e
raiyuvraj <yuvirai017@gmail.com>
2022-12-06 18:09:27 -0800

    [Yuvraj]- PartTime

--- EmplyeeWedge/Program.cs
+++ EmplyeeWedge/Program.cs
@@ -6,14 +6,19 @@ class Program
     static void Main(string[] args)
     {
         int Full_Time = 1;
+        int Part_Time = 2;
         int Wedge_Rate = 20;
         int EmpHours = 0;
         int EmpWedge = 0;
         Random random = new Random();
-        int EmpCheck = random.Next(0,2);
+        int EmpCheck = random.Next(0,3);
         if (EmpCheck == Full_Time)
         {
             EmpHours = 8;
+        }
+        else if (EmpCheck == Part_Time)
+        {
+            EmpHours = 4;
         }
         else
         {
