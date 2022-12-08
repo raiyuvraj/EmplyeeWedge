de24722404f87bcb520302e635946d2f86347a67
raiyuvraj <yuvirai017@gmail.com>
2022-12-07 17:19:34 -0800

    [Yuvraj]- Month Salary

--- EmplyeeWedge/Program.cs
+++ EmplyeeWedge/Program.cs
@@ -6,28 +6,34 @@ class Program
     public const int Full_Time = 1;
     public const int Part_Time = 2;
     public const int Wedge_Rate = 20;
+    public const int Working_Days = 20;
 
     static void Main(string[] args)
     {
         int EmpHours = 0;
         int EmpWedge = 0;
-        Random random = new Random();
-        int EmpCheck = random.Next(0,3);
-        switch  (EmpCheck)
+        int TotalWedge = 0;
+        for  (int days = 0; days < Working_Days; days++)
         {
-            case Full_Time:
-                EmpHours = 8;
-                break;
-            case Part_Time:
-                EmpHours = 4;
-                break;
-            default:
-                EmpHours = 0;
-                break;
-
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 3);
+            switch (EmpCheck)
+            {
+                case Full_Time:
+                    EmpHours = 8;
+                    break;
+                case Part_Time:
+                    EmpHours = 4;
+                    break;
+                default:
+                    EmpHours = 0;
+                    break;
+            }
+            EmpWedge = EmpHours * Wedge_Rate;
+            TotalWedge += EmpWedge;
+            Console.WriteLine("Employee Daily Wage : " + EmpWedge);
         }
-        EmpWedge = EmpHours * Wedge_Rate;
-        Console.WriteLine("Employee Daily Wage : " + EmpWedge);
+        Console.WriteLine("Total Wedge for month" + TotalWedge);
     }
- 
+
 }
